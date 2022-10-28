b941fdeb7af59808cd7f263e334d3a430f8fc586
mangwi10 <mankwiyana@gmail.com>
2022-10-28 22:51:58 +0800

    Tugas 9 (DONE)

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Tugas7 from './tugas7/tugas7';
 import Tugas8 from './tugas8/tugas8';
+import Tugas9 from './tugas9/tugas9';
 
 function App() {
   return (
     <>
       <Tugas7/>
       <Tugas8 name="I Nyoman Wiyana" batch="39" email="mankwiyana@gmail.com"/>
+      <Tugas9/>
     </>
   );
 }
