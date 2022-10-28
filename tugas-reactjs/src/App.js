45d51c35661083984fb2254ce102c5a3cb1b22fc
mangwi10 <mankwiyana@gmail.com>
2022-10-28 13:02:33 +0800

    Tugas 7 - React: Introduction (DONE)

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+            <h1>Hi, Saya I Nyoman Wiyana</h1>
+            <p>Saya dari Batch 39 sudah berhasil menginstal React</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
