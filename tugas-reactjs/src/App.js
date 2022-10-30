f7e2fd21be08229ff2e9a7d616b7c4a7ec32712f
mangwi10 <mankwiyana@gmail.com>
2022-10-30 12:12:30 +0800

    Tugas 10 - React : UI with Tailwind CSS (DONE)

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 import Tugas7 from './tugas7/tugas7';
 import Tugas8 from './tugas8/tugas8';
 import Tugas9 from './tugas9/tugas9';
+import Tugas10 from './tugas10/tugas10';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Tugas7/>
       <Tugas8 name="I Nyoman Wiyana" batch="39" email="mankwiyana@gmail.com"/>
       <Tugas9/>
+      <Tugas10/>
     </>
   );
 }
