105259e1e4ee686d451b42bd401de72b970d595e
mangwi10 <mankwiyana@gmail.com>
2022-11-02 00:12:04 +0800

    Tugas 11 - React CRUD Bagian 1 (DONE)

--- tugas-reactjs/src/tugas7/tugas7.js
+++ tugas-reactjs/src/tugas7/tugas7.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Tugas7() {
     return ( 
-        <div class="container">
+        <div className='container'>
             <h1>Hi, Saya I Nyoman Wiyana</h1>
             <p>Saya dari Batch 39 sudah berhasil menginstal React</p>
     </div>
